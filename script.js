63cd2d0c69155aca50147fb240256715605030b3
mahamakhan <mahamimranamjad@gmail.com>
2022-10-31 14:50:13 -0700

    l

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 // TODO: [1] Select the <body>
+document.querySelector('body')
 
 // TODO: [2] Select the <header>
-
+document.querySelector('header')
 // TODO: [3] Select all of the elements with class "quote"
-
+document.querySelectorAll('.quote')
 // TODO: [4] Select all of the subject elements
-
+document.querySelectorAll('.subject')
 // TODO: [5] Select the navigation links
-
+document.querySelector('a')
 // TODO: [6] Select all the quotes in the "life" section.
+document.querySelector('.subject.life>article')
+document.querySelector('.life').querySelectorAll('.quote')
 
 // TODO: [7] Using relative selection, select the <p> element containing the second Mark Twain quote
-
+document.getElementById('mark-twain-second')
 // TODO: [8] Do the same using a query selector
-
+document.querySelector('#mark-twain-second').querySelector('p')
 // TODO: [9] Select the 'Quotes About Motivation' heading
-
+document.querySelectorAll('h2')[2]
 // TODO: [10] -  Add a class 'simplicity' to the first subject.
-
+document.querySelector('.subject').createClass = 'simplicity'
 
 // BONUS
 // TODO: [11] Create an element with the text of your favorite quote.
-
+document.querySelector('footer').createElement(h3).innerText =
+  'Read Books for quotes'
 // TODO: [12] Add the element that you created in the previous step to the layout by appending it to a corresponding subject section.
 
 // TODO: [13] Add the author's last name, as a class name, to one of the quotes.
